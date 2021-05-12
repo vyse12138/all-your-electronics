7c20d70230381b67a7537a749943d6769a65ada4
unknown <zhuyulei12333@gmail.com>
2021-05-12 18:45:24 +1000

    update

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: '../backend/public'
+  },
   server: {
     port: 3001
   },
