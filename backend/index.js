364a53d60d03106a090345abb6f588fe78c55e10
unknown <zhuyulei12333@gmail.com>
2021-05-12 23:10:13 +1000

    feat: sqlite db

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
+const sqlite3 = require('sqlite3').verbose()
+let accountDatabase = new sqlite3.Database('./databases/account.db')
 
-app.all('/*', function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*') // restrict it to the required domain
+// CORS
+app.all('/*', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
   res.header(
     'Access-Control-Allow-Headers',
@@ -14,15 +17,16 @@ app.all('/*', function (req, res, next) {
     next()
   }
 })
-
+app.use(express.json())
+// host front end pages
 app.use(express.static('public'))
 
-app.post('/user', function (req, res) {
-  res.send('login')
-  res.set('Access-Control-Allow-Origin', '*')
-  res.set('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+app.post('/api/login', (req, res) => {
+  res.send(req.body)
 })
 
 app.listen(3000, () => {
   console.log('project listening at http://localhost:$3000')
 })
+
+accountDatabase.close()
--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -12,7 +12,10 @@
     ></el-input>
     <br />
     <br />
-    <el-button @click="handleOnClick"> Login in </el-button>
+    <el-button type="primary" @click="handleLogin"> Login In </el-button>
+    <br />
+    <br />
+    <el-button type="success" @click="handleSignUp"> Or Sign Up </el-button>
   </div>
 </template>
 
@@ -21,10 +24,25 @@ import { ref } from 'vue'
 import axios from 'axios'
 const username = ref('')
 const password = ref('')
-const handleOnClick = () => {
-  axios.post('http://localhost:3000/').then((res) => {
-    console.log(res.data)
-  })
+const handleLogin = () => {
+  axios
+    .post('http://localhost:3000/api/login', {
+      username: username.value,
+      password: password.value
+    })
+    .then((res) => {
+      console.log(res.data)
+    })
+}
+const handleSignUp = () => {
+  axios
+    .post('http://localhost:3000/api/signup', {
+      username: username.value,
+      password: password.value
+    })
+    .then((res) => {
+      console.log(res.data)
+    })
 }
 </script>
 
